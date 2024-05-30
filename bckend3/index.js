a54a8e9442c254a78f7e885319fea46f93e0d08f
2003devansh <faujdardevansh@gmail.com>
2024-05-30 14:04:33 +0530

    commiting changes

--- bckend3/index.js
+++ bckend3/index.js
@@ -15,8 +15,30 @@
 
  const express  = require("express")
  const app  = express() ; 
+
+
+ //middleware ka code :- 
+ // phele yeh  chalega fir rouutes
+
+app.use((req,res,next)=>{
+    console.log("middlewae chala");
+    next() ; 
+})
+
+ //creation of route
  app.get("/" , (req, res)=>{
     res.send("hello world")
+ })
+ // (req, res)  yeh ek middleware hai 
+
+ // middleware :- kisi bhi route ko chalane se phele hum koi operation perform karna chate hai 
+ // to middleware use aata hai 
+
+
+ // error handeling 
+ app.use((err, req ,res ,next)=>{
+    console.error(err.stack)
+    res.satus(500).send("something broke !!")
  })
 
  app.listen(3000)

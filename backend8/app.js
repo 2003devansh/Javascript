c3f8b8db5a1b4c4cfe518b9121783ba483f5c721
2003devansh <faujdardevansh@gmail.com>
2024-06-13 15:21:12 +0530

    commit it

--- backend8/app.js
+++ backend8/app.js
@@ -1,9 +1,34 @@
+
+
+const cookieParser = require("cookie-parser");
 const express  = require("express") ;
 const app  = express() ;
+const bcrypt = require('bcrypt');
+
+app.use(cookieParser()) ;
 
 
 app.get("/" , (req,res)=>{
+    res.cookie("name " , "devansh")
     res.send("heyy")
+  
+})
+
+app.get("/read" , function(req,res){
+   
+    console.log(req.cookies);
+    res.send("read page")
+})
+
+app.get("/im" , (req,res)=>{
+
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash("hi there", salt, function(err, hash) {
+           console.log(hash);
+        });
+    });
+    // yeh password ko hash mai convert kar dega 
+    
 })
 
 app.listen(3000)

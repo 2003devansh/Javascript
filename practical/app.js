bb91ce68a13d32285a7ac2a1c451f9d6f6f85b82
2003devansh <faujdardevansh@gmail.com>
2024-09-10 00:43:54 +0530

    authorisation

--- practical/app.js
+++ practical/app.js
@@ -5,6 +5,7 @@ const  bcrypt = require('bcrypt') ;
 const cookieParser = require('cookie-parser');  // Only declared once
 const path = require('path');
 const jwt = require('jsonwebtoken') ;
+const { log } = require("console");
 
 
 
@@ -35,5 +36,25 @@ app.post('/create', async (req, res) => {
         })
     })
 });
+
+app.get("/login", (req,res)=>{
+    res.render('login')
+})
+
+app.get("login", async (req,res)=>{
+    let  user = await userModel.findOne({email : req.body.email}) ;
+    if(!user) return res.send("something went wrong") ;
+
+    bcrypt.compare(req.body.password , user.password , (err,result)=>{
+    if(result)   res.send("yes you can login")
+        
+    })
+})
+
+
+app.get("/logout" , (req,res)=>{
+    res.cookie("token","");
+    res.redirect("/"); 
+})
 
 app.listen(3000);
